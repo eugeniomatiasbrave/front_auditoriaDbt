fab3db257ae6343d536624ed734c6e7ca4fdbf5b
Eugenio Matias Brave <eugenio_m_brave@hotmail.com>
2025-10-17 12:32:14 -0300

    login mejora, + deuda tecnica con alerts - usar sweetalert

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,63 @@
+import { useForm } from "react-hook-form";
+import { postData, ApiResponse, ApiError } from '../../services/axiosService.js';
+import { endpoints } from "../../services/endpoints";
+import axios from "axios";
+
+
 
 export const Login = () => {
-	  return (
-		<div>
-			<h2 className="text-2xl text-gray-800 font-bold mb-4">Iniciar Sesión</h2>
-			{/* Aquí puedes agregar más contenido para la página de inicio de sesión */}
+
+	const { register, handleSubmit, reset } = useForm();
+
+	return (
+		<div className="max-w-md mx-auto mt-12">
+			<h2 className="text-2xl text-sky-800 font-bold mb-4">Iniciar Sesión</h2>
+			<form
+				className="bg-white p-6 rounded shadow-md space-y-4 "
+				onSubmit={handleSubmit(async (values) => {
+					try {
+						const response = await postData<ApiResponse>(endpoints.LOGIN, values);
+
+						if (!response.success || response.data === null) {
+							alert(response.message || "Credenciales inválidas");
+							return;
+						}
+						reset();
+						alert("Login exitoso");
+						//console.log("Usuario logueado:", response);
+					} catch (error: unknown) {
+						// Captura el error lanzado por Axios
+						if (axios.isAxiosError(error)) {
+							const apiError = error.response?.data as ApiError | undefined;
+							alert(apiError?.message || "Error inesperado");
+						} else {
+							alert("Error inesperado");
+						}
+					}
+				})} >
+				<div>
+					<label className="block text-gray-700">Email</label>
+					<input type="email"
+						{...register("email", { required: "Este campo es obligatorio", pattern: { value: /^\S+@\S+$/i, message: "Email inválido" } })}
+						className="text-black mt-1 block w-full border border-gray-300 rounded-md p-2"
+						placeholder="Ingrese su email"
+						autoComplete="off"
+					/>
+				</div>
+				<div>
+					<label className="block text-gray-700">Contraseña</label>
+					<input type="password"
+						{...register("password", { required: "Este campo es obligatorio" })}
+						className="text-black mt-1 block w-full border border-gray-300 rounded-md p-2"
+						placeholder="Ingrese su contraseña"
+						autoComplete="off"
+					/>
+
+				</div>
+				<button type="submit" className="w-full bg-sky-600 font-bold text-white p-2 rounded">
+					Iniciar Sesión
+				</button>
+			</form>
 		</div>
-	  )
+	);
 }
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -12,9 +12,9 @@ export const Register = () => {
 		<div className="max-w-md mx-auto mt-12">
 			<h2 className="text-2xl text-sky-800 font-bold mb-4">Registro</h2>
 			<form
-			
+
 				className="bg-white p-6 rounded shadow-md space-y-4 "
-				onSubmit={handleSubmit( async (values) => {
+				onSubmit={handleSubmit(async (values) => {
 					const response = await postData<ApiResponse>(endpoints.REGISTER, values);
 					console.log(response);
 				})} >
@@ -22,11 +22,11 @@ export const Register = () => {
 					<label className="block text-gray-700">Nombre de usuario</label>
 					<input
 						type="text"
-						{...register("firstName", { required: "Este campo es obligatorio" } )}
-						className=" text-black mt-1 block w-full border border-gray-300 rounded-md p-2"
-					  	placeholder="Ingrese su nombre de usuario"	
+						{...register("firstName", { required: "Este campo es obligatorio" })}
+						className="text-black mt-1 block w-full border border-gray-300 rounded-md p-2 focus:bg-white focus:border-sky-600 focus:outline-none"
+						placeholder="Ingrese su nombre de usuario"
+						autoComplete="off"
 					/>
-
 				</div>
 				<div>
 					<label className="block text-gray-700">Apellido</label>
@@ -35,29 +35,29 @@ export const Register = () => {
 						{...register("lastName", { required: "Este campo es obligatorio" })}
 						className="text-black mt-1 block w-full border border-gray-300 rounded-md p-2"
 						placeholder="Ingrese su apellido"
+						autoComplete="off"
 					/>
-
 				</div>
 				<div>
 					<label className="block text-gray-700">Email</label>
-					<input type="email" 
+					<input type="email"
 						{...register("email", { required: "Este campo es obligatorio", pattern: { value: /^\S+@\S+$/i, message: "Email inválido" } })}
 						className="text-black mt-1 block w-full border border-gray-300 rounded-md p-2"
 						placeholder="Ingrese su email"
-
+					    autoComplete="off"
 					/>
-
 				</div>
 				<div>
 					<label className="block text-gray-700">Contraseña</label>
-					<input type="password" 
+					<input type="password"
 						{...register("password", { required: "Este campo es obligatorio" })}
 						className="text-black mt-1 block w-full border border-gray-300 rounded-md p-2"
 						placeholder="Ingrese su contraseña"
+						autoComplete="off"
 					/>
 
 				</div>
-				<button type="submit" className="w-full bg-sky-600 text-white p-2 rounded">
+				<button type="submit" className="w-full bg-sky-600 font-bold text-white p-2 rounded">
 					Registrarse
 				</button>
 			</form>
