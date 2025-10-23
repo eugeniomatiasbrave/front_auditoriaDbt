8bc790fdca8bbb925d8fa34880ea5f26ad52cbdd
Eugenio Matias Brave <eugenio_m_brave@hotmail.com>
2025-10-23 13:17:05 -0300

    ruta de railway

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+  BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://braveauditoriadbt-production.up.railway.app",
   TIMEOUT: 8000, // 8 segundos
   RETRY_ATTEMPTS: 3, // Número de reintentos para peticiones fallidas
 } as const;
